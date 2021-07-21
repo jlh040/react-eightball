e90936a211691b5b42f3ce809dfe370ecdf5c7b3
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-21 11:55:37 -0700

    the text now changes color when it is clicked

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 
 const EightBall = ( { answers } ) => {
+  let randomAnswer;
   const [message, setMessage] = useState('Think of a Question');
   const [color, setColor] = useState('black');
   const changeMsgAndCol = () => {
-    const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+    randomAnswer = answers[Math.floor(Math.random() * answers.length)];
     setMessage(randomAnswer.msg);
     setColor(randomAnswer.color);
   }
+
   return (
-    <div className="EightBall" onClick={() => changeMsgAndCol()}>
-      <li>message: {message}</li>
-      <li>color: {color}</li>
+    <div className="EightBall" onClick={() => changeMsgAndCol()} style={{color: color}}>
+      {message}
     </div>
   )
-
 }
 
 export default EightBall;
