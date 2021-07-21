9ce407c5d68372bd7ee79e1d4cabb54eaeb2f030
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-21 11:52:09 -0700

    the EightBall component now displays a new message and color when it is clicked

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,4 +1,19 @@
-const EightBall = () => {
+import { useState } from 'react';
+
+const EightBall = ( { answers } ) => {
+  const [message, setMessage] = useState('Think of a Question');
+  const [color, setColor] = useState('black');
+  const changeMsgAndCol = () => {
+    const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+    setMessage(randomAnswer.msg);
+    setColor(randomAnswer.color);
+  }
+  return (
+    <div className="EightBall" onClick={() => changeMsgAndCol()}>
+      <li>message: {message}</li>
+      <li>color: {color}</li>
+    </div>
+  )
 
 }
 
